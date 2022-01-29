a8607bce0e4bb55f41eb5c20d7f078ea0e0f7e45
AlexisAddeco <AlexisAddeco@LAPTOP-CEBC58TI>
2022-01-29 12:56:55 -0500

    Clase de condicionales

---
+++ SeleniumCursoEnero/src/test/java/com/java/Condicionales.java
@@ -0,0 +1,22 @@
+package com.java;
+
+public class Condicionales {
+
+	public static void main(String[] args) {
+		// Automovil conduciendo a exceso de velocidad
+		
+		// IF 
+		
+		// IF ELSE
+		
+		// IF Anidado
+		
+		// IF ELSE IF
+		
+		// SWITCH CASE 
+		
+		
+
+	}
+
+}
